1ef8a54bb2819e72be749be6b8546d210aa3c16e
joySutradhor <joysutradhorcmt@gmail.com>
2025-04-05 00:32:59 +0600

    jsx updated

--- src/ClientSide/WhatsNew/WhatsNew.jsx
+++ src/ClientSide/WhatsNew/WhatsNew.jsx
@@ -2,7 +2,7 @@ import FreeWebinar from "../../components/WhatsNew/FreeWebinar";
 import UpCmnTtle from "../../components/WhatsNew/UpCmnTtle";
 import Footer from "../Shared/Footer/Footer";
 import Navbar from "../Shared/Navbar/Navbar";
-import UpcommingMicroEvents from "./upcommingMicroEvents";
+import UpcommingMicroEvents from "./upcommingMicroEvents.jsx";
 
 import WhatsNewHero from "./WhatsNewHero";
 
