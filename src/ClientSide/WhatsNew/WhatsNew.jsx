5d6599fe53082a28742038b96e65568945d8bc18
joySutradhor <joysutradhorcmt@gmail.com>
2025-04-05 00:45:14 +0600

    check

--- src/ClientSide/WhatsNew/WhatsNew.jsx
+++ src/ClientSide/WhatsNew/WhatsNew.jsx
@@ -2,7 +2,8 @@ import FreeWebinar from "../../components/WhatsNew/FreeWebinar";
 import UpCmnTtle from "../../components/WhatsNew/UpCmnTtle";
 import Footer from "../Shared/Footer/Footer";
 import Navbar from "../Shared/Navbar/Navbar";
-import UpcommingMicroEvents from "./UpcommingMicroEvents.jsx";
+import UpcommingMicroEvents from "./UpcommingMicroEvents";
+
 
 
 import WhatsNewHero from "./WhatsNewHero";
@@ -15,7 +16,7 @@ const WhatsNew = () => {
 
 			<div className="mt-[110px] md:mt-[80px]   lg:mt-[80px] xl:mt-[100px] 2xl:mt-[100px]">
 			<UpCmnTtle />
-				<UpcommingMicroEvents />
+				<UpcommingMicroEvents></UpcommingMicroEvents>
 			</div>
 
 			<FreeWebinar />
